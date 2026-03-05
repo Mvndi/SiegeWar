d284f7c3ef3c0f219d47ecd1afae654b07d8672e
HydrolienF <hydrolien.f@gmail.com>
2026-03-05 22:09:05 +0100

    Fix onSiegeEnd if test & react to the 4 towny events.

--- src/main/java/com/gmail/goosius/siegewar/listeners/MvndiPillageListener.java
+++ src/main/java/com/gmail/goosius/siegewar/listeners/MvndiPillageListener.java
@@ -7,6 +7,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import com.palmergames.bukkit.towny.event.actions.TownyActionEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyBuildEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyDestroyEvent;
+import com.palmergames.bukkit.towny.event.actions.TownyItemuseEvent;
+import com.palmergames.bukkit.towny.event.actions.TownySwitchEvent;
 import com.palmergames.bukkit.towny.object.Town;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,17 +22,35 @@ public class MvndiPillageListener implements Listener {
     }
     @EventHandler(ignoreCancelled = true)
     public void onSiegeEnd(SiegeEndEvent event) {
-        // If the attacker won
-        if(event.getSiegeWinner() != null && event.getSiegeWinner().equals(event.getAttackerName())) {
+        if ("ATTACKERS".equals(event.getSiegeWinner())) {
             startPillaging(event.getSiege().getTown());
         } else {
-            SiegeWar.info("No pillage for " + event.getSiege().getTown().getName());
+            SiegeWar.info("No pillage for " + event.getSiege().getTown().getName()+ " because siege was won by " + event.getSiegeWinner() + " and not by " + event.getAttackerName());
         }
     }
 
     // Uncancel each towny event in a pillaging town
     @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
-    public void onBuildInPillagingTown(TownyActionEvent event) {
+    public void onBuildInPillagingTown(TownySwitchEvent event) {
+        onTownyEvent(event);
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
+    public void onBuildInPillagingTown(TownyBuildEvent event) {
+        onTownyEvent(event);
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
+    public void onBuildInPillagingTown(TownyDestroyEvent event) {
+        onTownyEvent(event);
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.HIGHEST)
+    public void onBuildInPillagingTown(TownyItemuseEvent event) {
+        onTownyEvent(event);
+    }
+
+    private void onTownyEvent(TownyActionEvent event) {
         if(!event.isCancelled()){
             return;
         }
