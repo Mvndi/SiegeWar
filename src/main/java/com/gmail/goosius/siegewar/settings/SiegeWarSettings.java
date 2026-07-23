409f374f30216c85ea8ea6e0c93f7d79c3b60668
abanana23 <booman12345@proton.me>
2026-07-23 23:31:52 +0300

    fix

--- src/main/java/com/gmail/goosius/siegewar/settings/SiegeWarSettings.java
+++ src/main/java/com/gmail/goosius/siegewar/settings/SiegeWarSettings.java
@@ -536,6 +536,10 @@ public class SiegeWarSettings {
 			}
 		}
 
+		if (isCapital && defenderNation.getNumTowns() <= 1) {
+			return null;
+		}
+
 		boolean capitalWeek = isCapitalWeek();
 
 		if (capitalWeek != isCapital) {
@@ -544,8 +548,7 @@ public class SiegeWarSettings {
 					: Translatable.of("msg_err_only_non_capitals_can_be_sieged_this_week");
 		}
 
-		if (isCapital && attacker instanceof Nation attackerNation && defenderNation != null && !defenderNation.equals(attackerNation)
-				&& defenderNation.getNumTowns() > 1) {
+		if (isCapital && attacker instanceof Nation attackerNation && !defenderNation.equals(attackerNation)) {
 			int required = getRequiredTownWinsForCapitalSiege(defenderNation.getLevelNumber());
 			int actual = NationMetaDataController.getTownWeekSiegeWins(attackerNation, defenderNation);
 			if (actual < required) {
